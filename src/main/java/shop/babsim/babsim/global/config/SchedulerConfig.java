d5c642071e5165744dce3a8833bbd9b76ea7e221
inhooo00 <chldlsgh0987@naver.com>
2025-04-02 23:34:12 +0900

    Feature(#9): config 설정

---
+++ src/main/java/shop/babsim/babsim/global/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package shop.babsim.babsim.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
