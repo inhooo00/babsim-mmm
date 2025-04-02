66b5c181e88617014d7afd9bc25dede487895689
inhooo00 <chldlsgh0987@naver.com>
2025-04-03 00:18:28 +0900

    Feature(#8): SwaggerConfig 설정

---
+++ src/main/java/shop/babsim/babsim/global/config/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package shop.babsim.babsim.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.ArrayList;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI nongDamAPI() {
+        Info info = new Info()
+                .title("밥심(mmm) API")
+                .description("밥심(mmm) API 명세서")
+                .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        Server localServer = new Server()
+                .url("https://dwenoeim.store")
+                .url("http://localhost:8080")
+                .description("Local Server");
+
+        return new OpenAPI()
+                .info(info)
+                .servers(Collections.singletonList(localServer))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    public SwaggerConfig(MappingJackson2HttpMessageConverter converter) {
+        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+    }
+}
