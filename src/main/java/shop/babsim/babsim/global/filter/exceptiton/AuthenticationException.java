7e237da2778039801a649d2a6aeb26a90c0ae785
inhooo00 <chldlsgh0987@naver.com>
2025-04-03 14:29:20 +0900

    Feature(#12): JWT Filter 로직 구현

---
+++ src/main/java/shop/babsim/babsim/global/filter/exceptiton/AuthenticationException.java
@@ -0,0 +1,14 @@
+package shop.babsim.babsim.global.filter.exceptiton;
+
+import shop.babsim.babsim.global.error.exception.AuthGroupException;
+
+public class AuthenticationException extends AuthGroupException {
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+
+    public AuthenticationException() {
+        this("인증에 실패했습니다. 자격 증명을 확인하고 다시 시도하십시오.");
+    }
+}
