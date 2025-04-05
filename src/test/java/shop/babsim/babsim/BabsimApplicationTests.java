2da2607c89e60d504e01f93bff7fc9c03fcdf1ef
inhooo00 <chldlsgh0987@naver.com>
2025-04-05 23:52:11 +0900

    Test(#17): TokenServiceTest 작성

--- src/test/java/shop/babsim/babsim/BabsimApplicationTests.java
+++ src/test/java/shop/babsim/babsim/BabsimApplicationTests.java
@@ -2,8 +2,8 @@ package shop.babsim.babsim;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
 class BabsimApplicationTests {
 
     @Test
