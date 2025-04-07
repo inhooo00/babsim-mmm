6e37b590c856d3fa08e4f5a4fadcd82669eb50d4
inhooo00 <chldlsgh0987@naver.com>
2025-04-08 01:55:18 +0900

    Feature(#19): reader, writer를 사용하기 위한 SimpleJobConfiguration 설계

---
+++ src/main/java/shop/babsim/babsim/global/config/SimpleJobConfiguration.java
@@ -0,0 +1,41 @@
+package shop.babsim.babsim.global.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import shop.babsim.babsim.place.csv.CsvReader;
+import shop.babsim.babsim.place.csv.CsvScheduleWriter;
+import shop.babsim.babsim.place.csv.dto.PlaceCsvData;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class SimpleJobConfiguration {
+    private final CsvReader csvReader;
+    private final CsvScheduleWriter csvScheduleWriter;
+
+    @Bean
+    public Job shopDataLoadJob(JobRepository jobRepository, Step shopDataLoadStep) {
+        return new JobBuilder("shopInformationLoadJob", jobRepository)
+                .start(shopDataLoadStep)
+                .build();
+    }
+
+    @Bean
+    public Step shopDataLoadStep(
+            JobRepository jobRepository,
+            PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("shopDataLoadStep", jobRepository)
+                .<PlaceCsvData, PlaceCsvData>chunk(100, platformTransactionManager)
+                .reader(csvReader.csvScheduleReader())
+                .writer(csvScheduleWriter)
+                .build();
+    }
+}
