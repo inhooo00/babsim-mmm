58a59a99d20d8aa9d877bfec4172e6a25378bd1c
inhooo00 <chldlsgh0987@naver.com>
2025-04-08 22:07:11 +0900

    Feature(#28): AmazonS3Controller 파일 구현

---
+++ src/main/java/shop/babsim/babsim/review/s3/api/AmazonS3Controller.java
@@ -0,0 +1,32 @@
+package shop.babsim.babsim.review.s3.api;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import shop.babsim.babsim.review.s3.application.AwsS3Service;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/file")
+public class AmazonS3Controller {
+
+    private final AwsS3Service awsS3Service;
+
+    @PostMapping
+    public ResponseEntity<List<String>> uploadFile(List<MultipartFile> multipartFiles){
+        return ResponseEntity.ok(awsS3Service.uploadFile(multipartFiles));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteFile(@RequestParam String fileName){
+        awsS3Service.deleteFile(fileName);
+
+        return ResponseEntity.ok(fileName);
+    }
+}
