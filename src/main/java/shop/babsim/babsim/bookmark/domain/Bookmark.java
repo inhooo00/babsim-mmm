cde8b2a15257c3c46448fbb44d625ab76b673875
inhooo00 <chldlsgh0987@naver.com>
2025-04-08 22:26:36 +0900

    Feature(#30): Bookmark Entity 생성

---
+++ src/main/java/shop/babsim/babsim/bookmark/domain/Bookmark.java
@@ -0,0 +1,29 @@
+package shop.babsim.babsim.bookmark.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.babsim.babsim.global.entity.BaseEntity;
+import shop.babsim.babsim.member.domain.Member;
+import shop.babsim.babsim.place.domain.Place;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Bookmark extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id")
+    private Place place;
+}
