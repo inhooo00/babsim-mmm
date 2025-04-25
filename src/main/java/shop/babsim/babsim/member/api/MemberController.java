c49d61de54ee43488e4fb714a2d0a0a5d87e0222
inhooo00 <chldlsgh0987@naver.com>
2025-04-26 02:46:38 +0900

    Feature(#47): 마이페이지 조회 controller 코드 작성

--- src/main/java/shop/babsim/babsim/member/api/MemberController.java
+++ src/main/java/shop/babsim/babsim/member/api/MemberController.java
@@ -1,11 +1,25 @@
 package shop.babsim.babsim.member.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shop.babsim.babsim.global.annotation.CurrentUserEmail;
+import shop.babsim.babsim.global.template.RspTemplate;
+import shop.babsim.babsim.member.api.dto.response.MyPageInfoResDto;
+import shop.babsim.babsim.member.application.MemberService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/my-page")
+    public RspTemplate<MyPageInfoResDto> myProfileInfo(@CurrentUserEmail String email) {
+        MyPageInfoResDto memberResDto = memberService.findMyProfileByEmail(email);
+        return new RspTemplate<>(HttpStatus.OK, "내 프로필 정보", memberResDto);
+    }
 }
