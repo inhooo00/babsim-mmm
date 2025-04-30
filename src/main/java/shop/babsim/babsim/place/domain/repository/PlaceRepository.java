499efac571fb8484e7b9937c07a1a7353dddfa37
inhooo00 <chldlsgh0987@naver.com>
2025-04-30 18:03:26 +0900

    Devops(#53): Elasticsearch 로직 -> 기본 crud 로직으로 수정

--- src/main/java/shop/babsim/babsim/place/domain/repository/PlaceRepository.java
+++ src/main/java/shop/babsim/babsim/place/domain/repository/PlaceRepository.java
@@ -1,7 +1,12 @@
 package shop.babsim.babsim.place.domain.repository;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import shop.babsim.babsim.place.api.dto.response.PlaceSearchResDto;
 import shop.babsim.babsim.place.domain.Place;
 
 public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceCustomRepository {
@@ -9,4 +14,13 @@ public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceCustom
     Optional<Place> findByBusinessName(String businessName);
 
     Optional<Place> findByPlaceId(String placeId);
+
+    @Query("""
+                SELECT p.businessName AS businessName, p.placeId AS placeId
+                FROM Place p
+                WHERE LOWER(p.menu1) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                   OR LOWER(p.menu2) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                   OR LOWER(p.businessName) LIKE LOWER(CONCAT('%', :keyword, '%'))
+            """)
+    Page<PlaceSearchResDto> searchByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
--- src/main/java/shop/babsim/babsim/place/domain/repository/elasticsearch/PlaceSearchRepository.java
+++ src/main/java/shop/babsim/babsim/place/domain/repository/elasticsearch/PlaceSearchRepository.java
@@ -1,9 +1,9 @@
-package shop.babsim.babsim.place.domain.repository.elasticsearch;
-
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import shop.babsim.babsim.place.domain.PlaceDocument;
-
-public interface PlaceSearchRepository extends ElasticsearchRepository<PlaceDocument, Long>
-        ,PlaceSearchCustomRepository {
-}
+//package shop.babsim.babsim.place.domain.repository.elasticsearch;
+//
+//
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//import shop.babsim.babsim.place.domain.PlaceDocument;
+//
+//public interface PlaceSearchRepository extends ElasticsearchRepository<PlaceDocument, Long>
+//        ,PlaceSearchCustomRepository {
+//}
