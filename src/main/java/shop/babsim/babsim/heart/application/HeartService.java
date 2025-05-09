46983ef95de9b37b54df0ca07402821797a42e22
inhooo00 <chldlsgh0987@naver.com>
2025-05-09 16:31:08 +0900

    Feature(#59): 좋아요 누를 때 알림 발송

--- src/main/java/shop/babsim/babsim/heart/application/HeartService.java
+++ src/main/java/shop/babsim/babsim/heart/application/HeartService.java
@@ -7,6 +7,7 @@ import shop.babsim.babsim.heart.domain.repository.HeartRepository;
 import shop.babsim.babsim.member.domain.Member;
 import shop.babsim.babsim.member.domain.repository.MemberRepository;
 import shop.babsim.babsim.member.exception.MemberNotFoundException;
+import shop.babsim.babsim.notification.application.NotificationService;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,14 +16,20 @@ public class HeartService {
 
     private final HeartRepository heartRepository;
     private final MemberRepository memberRepository;
+    private final NotificationService notificationService;
 
     @Transactional
     public void toggleReviewHeart(String email, Long reviewId) {
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(MemberNotFoundException::new);
+        Member reviewer = memberRepository.findByReviewId(reviewId)
+                .orElseThrow(MemberNotFoundException::new);
 
-        if (!heartRepository.existsByMemberAndReviewId(member, reviewId)) {
+        boolean isLiked = !heartRepository.existsByMemberAndReviewId(member, reviewId);
+
+        if (isLiked) {
             heartRepository.addReviewHeart(member, reviewId);
+            notificationService.send(reviewer, member.getName() + "님이 리뷰에 좋아요를 눌렀습니다.");
             return;
         }
 
