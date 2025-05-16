51ed567451083d646d6917d4f22eded225360ad1
inhooo00 <chldlsgh0987@naver.com>
2025-05-16 16:04:53 +0900

    Modify(#67): Grade 값도 조회하도록 수정

--- src/main/java/shop/babsim/babsim/member/api/dto/response/MyPageInfoResDto.java
+++ src/main/java/shop/babsim/babsim/member/api/dto/response/MyPageInfoResDto.java
@@ -1,6 +1,7 @@
 package shop.babsim.babsim.member.api.dto.response;
 
 import lombok.Builder;
+import shop.babsim.babsim.member.domain.Grade;
 import shop.babsim.babsim.member.domain.Member;
 
 @Builder
@@ -9,16 +10,19 @@ public record MyPageInfoResDto(
         String nickName,
         Integer reviewCount,
         Double ratingAverage,
-        Integer reportCount
+        Integer reportCount,
+        String grade
 ) {
     public static MyPageInfoResDto of(Member member, Integer reviewCount,
                                       Double ratingAverage, Integer reportCount) {
+        Grade grade = Grade.getGradeByReviewCount(reviewCount);
         return MyPageInfoResDto.builder()
                 .picture(member.getPicture())
                 .nickName(member.getNickname())
                 .reviewCount(reviewCount)
                 .ratingAverage(ratingAverage)
                 .reportCount(reportCount)
+                .grade(grade.getName())
                 .build();
     }
 }
