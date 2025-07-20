76f89a60b5e6395855f640023d9db3817dca4b26
inhooo00 <chldlsgh0987@naver.com>
2025-07-20 17:41:16 +0900

    Modify(#118): 세종시는 db에 장소가 없기에 삭제

--- src/main/java/shop/babsim/babsim/place/domain/RegionGroup.java
+++ src/main/java/shop/babsim/babsim/place/domain/RegionGroup.java
@@ -8,7 +8,7 @@ public enum RegionGroup {
     INCHEON_GYEONGGI(List.of(Region.INCHEON, Region.GYEONGGI)),
     GANGWON_DAEGU(List.of(Region.GANGWON, Region.DAEGU)),
     GWANGJU_JEOLLA(List.of(Region.GWANGJU, Region.JEOLLABUK, Region.JEOLLANAM)),
-    DAEJEON_CHUNGCHEONG_SEJONG(List.of(Region.DAEJEON, Region.SEJONG, Region.CHUNGCHEONG_N, Region.CHUNGCHEONG_S)),
+    DAEJEON_CHUNGCHEONG_SEJONG(List.of(Region.DAEJEON, Region.CHUNGCHEONG_N, Region.CHUNGCHEONG_S)),
     BUSAN_ULSAN_GYEONGSANG(List.of(Region.BUSAN, Region.ULSAN, Region.GYEONGSANG_N, Region.GYEONGSANG_S)),
     JEJU(List.of(Region.JEJU));
 
