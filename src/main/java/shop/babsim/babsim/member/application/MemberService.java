0d97d5224503cf2549a552e9c5c12adf72f25568
inhooo00 <chldlsgh0987@naver.com>
2025-07-23 13:27:25 +0900

    Refactor(126): 등급 계산과 프로필 뱃지가 안 맞는 오류 수정

--- src/main/java/shop/babsim/babsim/member/application/MemberService.java
+++ src/main/java/shop/babsim/babsim/member/application/MemberService.java
@@ -71,7 +71,7 @@ public class MemberService {
 
     private List<ProfileInfoResDto> generateProfiles(int reviewCount) {
         return List.of(
-                createProfile(1, "밥심", reviewCount >= 0),
+                createProfile(1, "밥심", reviewCount >= -1),
                 createProfile(2, "쌀알", reviewCount >= 0),
                 createProfile(3, "밥그릇", reviewCount >= 3),
                 createProfile(4, "맛잘알", reviewCount >= 10),
