a9bff8d976ca4677c2238ac8d8faecc64ff46c47
inhooo00 <chldlsgh0987@naver.com>
2025-07-27 21:39:23 +0900

    Setting(#7): switch 파일 $ 못읽는 오류 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,7 +9,7 @@ function switch_proxy() {
 
     echo "> 전환할 port: $IDLE_PORT"
     echo "> Port 전환"
-    echo "set \\$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc # 엔진엑스가 변경할 프록시 주소를 생성하여 service-url.inc로 덮어 씁니다.
+    echo "set \$SERVICE_URL http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
     echo "> 엔진엑스 Reload"
     sudo systemctl reload nginx
